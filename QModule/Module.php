90299d319fb520aa59c986c09fc7d15140f778b6
a.ugryumov <megasounds@mail.ru>
2019-09-26 15:03:03 +0300

    no message

---
+++ QModule/Module.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace quarsintex\yii2\quartronic;
+
+/**
+ * Class Module
+ * @package amylabs\demo
+ */
+class Module extends \yupe\base\Module
+{
+    /**
+     *
+     */
+    const VERSION = '1.0';
+
+    /**
+     * @var string
+     */
+    public $controllerNamespace = 'quarsintex\yii2\quartronic\controllers';
+}
